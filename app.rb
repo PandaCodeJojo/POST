51e30206375e23e9aec6f3f4d533e30703b4c0b6
Jodi <jbrowne@nyc.yearup.org>
2019-01-02 14:57:42 -0500

    procfile heroku ready

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 
 
 # Run this script with `bundle exec ruby app.rb`
-require 'sqlite3'
+
 require 'active_record'
 
 #require model classes
@@ -18,7 +18,12 @@ require 'csv'
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
-ActiveRecord::Base.establish_connection(
+if ENV['DATABASE_URL']
+  require 'pg'
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else 
+  require 'sqlite3'
+  ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'db/development.db'
 )
--- database.rb
+++ database.rb
@@ -1,5 +1,5 @@
 # Run this script with `bundle exec database.rb`
-require 'sqlite3'
+
 require 'active_record'
 
 #require model classes
@@ -12,7 +12,12 @@ require 'csv'
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
-ActiveRecord::Base.establish_connection(
+if ENV['DATABASE_URL']
+  require 'pg'
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else 
+  require 'sqlite3'
+  ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'db/development.db'
 )
